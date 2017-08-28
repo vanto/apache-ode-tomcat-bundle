11c97a04ef17194f5791aeff6ea3179bbf3cea67
sathwik <sathwik@apache.org>
2017-08-28 16:32:33 +0530

    Hibernate logging with slf4j

--- src/main/resources/tomcat/bin/setenv.sh
+++ src/main/resources/tomcat/bin/setenv.sh
@@ -1,1 +1,1 @@
-CATALINA_OPTS="-Dbtm.root=$CATALINA_HOME -Dbitronix.tm.configuration=$CATALINA_HOME/conf/btm-config.properties"
+CATALINA_OPTS="-Dbtm.root=$CATALINA_HOME -Dbitronix.tm.configuration=$CATALINA_HOME/conf/btm-config.properties -Dorg.jboss.logging.provider=slf4j"
